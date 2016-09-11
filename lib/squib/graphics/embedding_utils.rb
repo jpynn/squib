04a2ce214dd023408c96de4ad1a4a86fbc364afd
Andy Meneely <andy@se.rit.edu>
2016-09-10 21:44:28 -0400

    writing up a util method for getting indices

---
+++ lib/squib/graphics/embedding_utils.rb
@@ -0,0 +1,27 @@
+module Squib
+  class EmbeddingUtils
+
+    # Given a string and a bunch of keys, give us back a mapping of those keys
+    # to where those keys start, and where they end (in ranges)
+    #
+    # See the spec for expected outputs
+    def self.indices(str, keys)
+      map = {}
+      keys.each do |key|
+        map[key] ||= []
+        start = 0
+        while true
+          idx = str.index(key, start)
+          if idx.nil?
+            break; # done searching
+          else
+            map[key] << (idx..(idx + key.bytesize))
+            start = idx + 1
+          end
+        end
+      end
+      return map
+    end
+
+  end
+end
